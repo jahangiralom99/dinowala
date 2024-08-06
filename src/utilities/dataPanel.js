05bd96e37dcffb674502c1384384a08c2151bfee
jahangir <jahangiralomtuf99@gmail.com>
2024-08-06 16:43:20 +0600

    added Utilities file

---
+++ src/utilities/dataPanel.js
@@ -0,0 +1,12 @@
+const base_url = "https://ecommerce.ionicerp.xyz";
+
+const fetch_url = "https://erpmethods.vercel.app";
+
+const header = {
+  url: base_url,
+  api_secret: "bcc0df87d4b1a77",
+  api_key: "092867a6814bfc7",
+  "Content-Type": "application/json",
+};
+
+export {base_url, fetch_url, header};
